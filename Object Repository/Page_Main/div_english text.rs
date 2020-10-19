74f3b3625d6d6f1e88ba854f72b1f671a036d056
dmitriy <dmitriy@dmitriys-air>
2020-10-19 18:54:00 +0300

    testcase1

---
+++ Object Repository/Page_Main/div_english text.rs
@@ -0,0 +1,88 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_english text</name>
+   <tag></tag>
+   <elementGuidId>f75f2e7f-3edb-4e58-9803-72e9a586f0c2</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.imgtxtblock.left.imgtxtblock-left-29177</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='search'])[2]/following::div[8]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>imgtxtblock left imgtxtblock-left-29177</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+        
+            
+                
+                    
+                        
+                                                            
+                                                    
+                    
+                    
+                        
+                            
+                                
+                                english text
+                            
+                                                    
+                    
+                
+            
+        
+    </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[@class=&quot;is-adaptive&quot;]/body[@class=&quot;home EP platformpage-index-index&quot;]/div[@class=&quot;d-table w-100 h-100&quot;]/div[@class=&quot;d-table-row&quot;]/div[@class=&quot;d-table-cell h-100&quot;]/main[@class=&quot;main top-margin&quot;]/div[@class=&quot;section-wrapper-bb&quot;]/div[@class=&quot;imgtxtblock left imgtxtblock-left-29177&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='search'])[2]/following::div[8]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='search'])[1]/following::div[9]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//main/div/div[2]</value>
+   </webElementXpaths>
+</WebElementEntity>
